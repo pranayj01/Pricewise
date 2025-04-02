7b0486633a7b8268c99bfda4c7616f8722e8ce52
Pranay Jain <pranayjain722@gmail.com>
2025-04-02 22:13:52 +0530

    first commit

---
+++ app/src/main/java/com/example/try2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.try2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
